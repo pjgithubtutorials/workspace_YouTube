d1f8df5f71e99547efe46514d36d33893def4470
PJalgotrader <jahangiry.pedram@gmail.com>
2022-03-10 23:06:08 -0700

    first commit

---
+++ Python/test.py
@@ -0,0 +1,26 @@
+
+print("Hello world!")
+
+def add(x,y):
+    return x+y
+
+print(add(10,15))
+
+
+x = 10
+
+
+
+even_number = [x for x in range(11) if x%2==0]
+print(even_number)
+
+
+
+
+import random
+import matplotlib.pyplot as plt
+
+plt.plot([random.randint(0,100) for _ in range(100) ])
+plt.show()
+
+
